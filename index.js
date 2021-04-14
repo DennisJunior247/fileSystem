be8b35c0907ce84d83ca960ef7f6cbf78ef9383a
dennisjunior247 <dennisjunior247@gmail.com>
2021-04-14 13:31:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+const getJsonData = async () => {
+  try {
+    const response = await fetch("http://jsonplaceholder.typicode.com/posts");
+    const jsonData = await response.json();
+
+    fs.appendFile("./result/result.json", JSON.stringify(jsonData), (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("awwn your file file has been appended");
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getJsonData();
